26c2ae49565b59792bc3fbedf6c3603474374b50
Robert-Zeelie <robert.zeelie@s4.co.za>
2022-09-20 10:58:00 +0200

    Add chrome options to run selenium in the host environment of replit

--- DB/ScrapperScripts/SeleniumScrapper.py
+++ DB/ScrapperScripts/SeleniumScrapper.py
@@ -29,6 +29,8 @@ def configChromeDriver():
 	# set driver for chrome
 	chrome_options = Options()
 	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--disable-dev-shm-usage")
+	chrome_options.add_argument("--no-sandbox")
 	global chromeDriver
 	chromeDriver = webdriver.Chrome(
 		ChromeDriverManager().install(), options=chrome_options)
